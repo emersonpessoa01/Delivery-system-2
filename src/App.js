7d4cc475c9c76aba7e08733cba53f12ef6826c66
emersonpessoa01 <eps011108@gmail.com>
2021-01-12 13:50:04 -0300

    added Main

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Form from "./components/form";
 
 import "./App.css";
+import Main from "./components/form/Main";
 
 const handleSubmit = (user) => {
   setTimeout(() => {
@@ -14,7 +15,9 @@ const initialValues = {};
 
 const App = () => (
   <div className="App">
-    <Form handleSubmit={handleSubmit} initialValues={initialValues} />
+    <Main>
+      <Form handleSubmit={handleSubmit} initialValues={initialValues} />
+    </Main>
   </div>
 );
 
---
+++ src/components/form/Main.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Main = styled.main`
+  background-color:#c1c1c1;
+  padding: 20px 30px;
+  border-radius: 5px;
+  opacity: .5s;
+
+
+`;
+export default Main;
