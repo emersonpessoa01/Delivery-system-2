9f7f3840b711c02784d2a4813e122a901d329842
emersonpessoa01 <eps011108@gmail.com>
2021-01-13 14:20:43 -0300

    updated

--- src/components/form/Main.js
+++ src/components/form/Main.js
@@ -1,17 +1,15 @@
 import styled from "styled-components";
 
 const Main = styled.main`
-  background-color:#b8cad4;
+  background-color: #b8cad4;
   padding: 20px 30px;
   border-radius: 5px;
-  opacity: .5s;
-  font-family: kollektif;
+  opacity: 0.5s;
+  /* font-family: kollektif; */
+  font-family: "Open Sans", sans-serif;
 
-  @media screen and (max-width:768px){
+  @media screen and (max-width: 768px) {
     width: 50%;
   }
-
-  
-
 `;
 export default Main;
