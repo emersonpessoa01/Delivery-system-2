335178ace133f50c3a1f9c830e23091c9d70922d
emersonpessoa01 <eps011108@gmail.com>
2021-01-14 17:19:27 -0300

    upload all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
 import React from "react";
 
-import Form from "./components/form";
+// import "./App.css";
 
-import "./App.css";
-import Main from "./components/form/Main";
+export default function App() {
+  return (
+    <body>
+      <div className="container">
+        <div className="content first-content">
+          <div className="first-column">
+            <h2 className="title">welcome back </h2>
+            <p className="description">To keep connect with us</p>
+            <p className="description">Please login with your personal info</p>
+            <button className="btn">sign in</button>
+          </div>
+          {/*first-column*/}
 
-const handleSubmit = (user) => {
-  setTimeout(() => {
-    console.log(user);
-    alert(JSON.stringify(user));
-  }, 500);
-};
-const initialValues = {};
+          <div className="second-column">
+            <h2 className="title">Create account</h2>
 
-const App = () => (
-  <div className="App">
-    <Main>
-      <Form handleSubmit={handleSubmit} initialValues={initialValues} />
-    </Main>
-  </div>
-);
+            <div className="social-media">
+              <ul>
+                <li>
+                  <a href="https://pt-br.facebook.com/login/">Facebook</a>
+                </li>
+                <li>
+                  <a href="https://www.linkedin.com/">Linkedin</a>
+                </li>
+                <li>
+                  <a href="https://myaccount.google.com/profile">Google+</a>
+                </li>
+              </ul>
+            </div>
+            {/*social-media */}
 
-export default App;
+            <p className="description">or use your email for registration:</p>
+            <form className="form">
+              <input type="text" placeholder="name" />
+              <input type="email" placeholder="email" />
+              <input type="password" placeholder="password" />
+              <button className="btn">Sign up</button>
+            </form>
+          </div>
+          {/*second-column*/}
+        </div>
+        {/*first-content*/}
+
+        <div className="content second-content">
+          <div className="first-column">
+            <h2 className="title">Hello, friend</h2>
+            <p className="description">Enter your personal details</p>
+            <p className="description">and start journey with us</p>
+            <button className="btn">Sign up</button>
+          </div>
+          {/*first-column*/}
+
+          <div className="second-column">
+            <h2 className="title">sign in to developer</h2>
+
+            <div className="social-media">
+              <ul>
+                <li>
+                  <a href="https://pt-br.facebook.com/login/">Facebook</a>
+                </li>
+                <li>
+                  <a href="https://www.linkedin.com/">Linkedin</a>
+                </li>
+                <li>
+                  <a href="https://myaccount.google.com/profile">Google+</a>
+                </li>
+              </ul>
+            </div>
+            {/*social-media */}
+
+            <p className="description">or use yput email for account</p>
+            <form className="form">
+              <input type="email " placeholder="email" />
+              <input type="password" placeholder="password" />
+              <a href="#">forgot your password</a>
+              <button className="btn">sign in</button>
+            </form>
+          </div>
+          {/*second-column*/}
+        </div>{/*second-content*/}
+      </div> 
+    </body>
+  );
+}
