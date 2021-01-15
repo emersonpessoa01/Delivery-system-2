9f4932ff1f2c41bde7f69aa5bed1532d70da6ae5
emersonpessoa01 <eps011108@gmail.com>
2021-01-15 13:33:52 -0300

    upload all

--- src/App.js
+++ src/App.js
@@ -4,85 +4,126 @@ import "./App.css";
 
 export default function App() {
   return (
-    <body>
-      <div className="container">
-        <div className="content first-content">
-          <div className="first-column">
-            <h2 className="title title-primary">Welcome back </h2>
-            <p className="description">To keep connect with us</p>
-            <p className="description">Please login with your personal info</p>
-            <button className="btn btn-primary">sign in</button>
-          </div>
-          {/*first-column*/}
-
-          <div className="second-column">
-            <h2 className="title">Create account</h2>
-
-            <div className="social-media">
-              <ul className="list-social-media">
-                <li>
-                  <a href="https://pt-br.facebook.com/login/">Facebook</a>
-                </li>
-                <li>
-                  <a href="https://www.linkedin.com/">Linkedin</a>
-                </li>
-                <li>
-                  <a href="https://myaccount.google.com/profile">Google+</a>
-                </li>
-              </ul>
-            </div>
-            {/*social-media */}
-
-            <p className="description">or use your email for registration:</p>
-            <form className="form">
-              <input type="text" placeholder="name" />
-              <input type="email" placeholder="email" />
-              <input type="password" placeholder="password" />
-              <button className="btn">Sign up</button>
-            </form>
-          </div>
-          {/*second-column*/}
+    <div className="container">
+      <div className="content first-content">
+        <div className="first-column">
+          <h2 className="title title-primary">Welcome back </h2>
+          <p className="description description-primary">
+            To keep connect with us
+          </p>
+          <p className="description description-primary">
+            Please login with your personal info
+          </p>
+          <button className="btn btn-primary">sign in</button>
         </div>
-        {/*first-content*/}
+        {/*first-column*/}
 
-        <div className="content second-content">
-          <div className="first-column">
-            <h2 className="title">Hello, friend</h2>
-            <p className="description">Enter your personal details</p>
-            <p className="description">and start journey with us</p>
-            <button className="btn">Sign up</button>
+        <div className="second-column">
+          <h2 className="title title-second">Create account</h2>
+
+          <div className="social-media">
+            <ul className="list-social-media">
+              <a className="link-social-media" href="https://pt-br.facebook.com/login/">
+                <li className="item-social-media">
+                  <i className="fab fa-facebook-f"></i>
+                </li>
+              </a>
+
+              <a className="link-social-media" href="https://www.linkedin.com/">
+                <li className="item-social-media">
+                  <i className="fab fa-linkedin-in"></i>
+                </li>
+              </a>
+
+              <a className="link-social-media" href="https://myaccount.google.com/profile">
+                <li className="item-social-media">
+                  <i className="fab fa-google-plus-g"></i>
+                </li>
+              </a>
+            </ul>
           </div>
-          {/*first-column*/}
+          {/*social-media */}
 
-          <div className="second-column">
-            <h2 className="title">sign in to developer</h2>
+          <p className="description description-second">
+            or use your email for registration:
+          </p>
+          <form className="form">
+            <label className="label-input" htmlFor="">
+              <i className="far fa-user icon-modify"></i>
+              <input autoFocus type="text" placeholder="name" />
+            </label>
 
-            <div className="social-media">
-              <ul>
-                <li>
-                  <a href="https://pt-br.facebook.com/login/">Facebook</a>
-                </li>
-                <li>
-                  <a href="https://www.linkedin.com/">Linkedin</a>
-                </li>
-                <li>
-                  <a href="https://myaccount.google.com/profile">Google+</a>
-                </li>
-              </ul>
-            </div>
-            {/*social-media */}
+            <label className="label-input" htmlFor="">
+              <i className="far fa-envelope icon-modify"></i>
+              <input type="email" placeholder="email" />
+            </label>
 
-            <p className="description">or use yput email for account</p>
-            <form className="form">
-              <input type="email " placeholder="email" />
+            <label className="label-input" htmlFor="">
+              <i className="fas fa-lock icon-modify"></i>
               <input type="password" placeholder="password" />
-              <a href="#">forgot your password</a>
-              <button className="btn">sign in</button>
-            </form>
+            </label>
+            <button className="btn btn-second">Sign up</button>
+          </form>
+        </div>
+        {/*second-column*/}
+      </div>
+      {/*first-content*/}
+
+      <div className="content second-content">
+        <div className="first-column">
+          <h2 className="title title-primary">Hello, friend</h2>
+          <p className="description description-primary">Enter your personal details</p>
+          <p className="description description-primary">and start journey with us</p>
+          <button className="btn btn-primary">Sign up</button>
+        </div>
+        {/*first-column*/}
+
+        <div className="second-column">
+          <h2 className="title title-second">sign in to developer</h2>
+
+          <div className="social-media">
+            <ul className="list-social-media">
+              <a className="link-social-media" href="https://pt-br.facebook.com/login/">
+                <li className="item-social-media">
+                  <i className="fab fa-facebook-f"></i>
+                </li>
+              </a>
+
+              <a className="link-social-media" href="https://www.linkedin.com/">
+                <li className="item-social-media">
+                  <i className="fab fa-linkedin-in"></i>
+                </li>
+              </a>
+
+              <a className="link-social-media" href="https://myaccount.google.com/profile">
+                <li className="item-social-media">
+                  <i className="fab fa-google-plus-g"></i>
+                </li>
+              </a>
+            </ul>
           </div>
-          {/*second-column*/}
-        </div>{/*second-content*/}
-      </div> 
-    </body>
+          {/*social-media */}
+
+          <p className="description description-second">or use your email for account</p>
+          <form className="form">
+
+            <label className="label-input" htmlFor="">
+              <i className="far fa-envelope icon-modify"></i>
+              <input type="email" placeholder="email" />
+            </label>
+
+            <label className="label-input" htmlFor="">
+              <i className="fas fa-lock icon-modify"></i>
+              <input type="password" placeholder="password" />
+            </label>
+
+            <a className="password" href="#">forgot your password</a>
+            <button className="btn btn-second">Sign in</button>
+          </form>
+        </div>
+        {/*second-column*/}
+      </div>
+      {/*second-content*/}
+    </div>
   );
 }
